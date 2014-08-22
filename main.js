f22fdab4fb342567c588000307b80bd3e4e7d518
Hank <henrydbissonnette@gmail.com>
2014-08-21 23:17:35 -0500

    initializeExercise

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 (function () {
-  exercise = require("./assets/js/exercises/hello_phillip.js");
+  openingExercise = require("./assets/js/exercises/hello_phillip.js");
   window.ENV = window.location.origin === "file://" ? "dev" : "prod";
 
   bindRun = function () {
@@ -11,17 +11,20 @@
     })
   }
 
-  init = function () {
-    bindRun();
-    editor = ace.edit('javascript-editor');
-    editor.setTheme('ace/theme/monokai');
-    editor.getSession().setMode('ace/mode/javascript');
-
+  initializeExercise = function (exercise) {
     setTimeout(function() {
       exercise.intro()
       }, 2000);
 
     testSuite = exercise.testSuite
+  }
+
+  init = function () {
+    bindRun();
+    editor = ace.edit('javascript-editor');
+    editor.setTheme('ace/theme/monokai');
+    editor.getSession().setMode('ace/mode/javascript');
+    initializeExercise(openingExercise);
   }
 
   window.addEventListener("load", init);
